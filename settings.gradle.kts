1ea8c53cda061a3217bc429da0b154d77df66e78
renggadiansa <renggadiansa@gmail.com>
2023-03-18 07:38:05 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Cross_Platform_Apps"
+include(":androidApp")
+include(":shared")
